4a0f6ff0675d1afea87aa2ef3e81e92a5bcf3735
Arthur <chaff.arthur@gmail.com>
2024-07-09 23:47:52 +0200

    wip

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-docker compose -f "docker-compose.yml" up -d --build
+docker swarm init
+docker build -t homepedia_app:latest ./app/
+docker stack deploy -c docker-stack.yml homepedia
+
+
+
+docker stack rm homepedia
+
+# docker compose -f "docker-compose.yml" up -d --build
